3573370c32824fea8883a5a77fac1e03cbcf5449
Malmi Bandara <it22277886@my.sliit.lk>
2025-03-30 02:11:56 +0530

    init Commit

---
+++ delivery_backend/src/test/java/CN_FODS/delivery_backend/DeliveryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package CN_FODS.delivery_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
